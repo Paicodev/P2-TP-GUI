c06677bf090010e40e7db9657f64243fa9aae6be
paicodev <paedmaag@gmail.com>
2026-05-13 19:43:02 -0300

    feat: Se realizaron los 4 puntos del primer ejercicio
    1- Se creo la ventana principal con el tamaño definido
    2- Se creo el contenedor JPanel y se lo añadio en la ventana principal.
    3- Dentro del panel, se añadio un JLabel con un texto para introducir un nombre y un JTextField con 20 columnas de ancho
    4- Se añadio un JButton con el texto "Saludar" y un JLabel inferior.
    
    -Por ultimo se añadieron los labels, botones, textos, etc al panel, y se definió como "Visible" a la ventana

--- P2-TP-GUI/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
+++ P2-TP-GUI/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.ejercicio1;
-
+import javax.swing.* ; //libreria principal de GUIs
 /**
  *
  * @author PC
@@ -11,6 +11,34 @@ package com.mycompany.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+        //1. Crea una ventana principal instanciando un JFrame y asígnale un tamaño usando setSize().
+        //instanciacion de frame
+        JFrame ventana = new JFrame();
+        ventana.setSize(800,800);
+        
+        //2. Crea un contenedor intermedio JPanel y añádelo a la ventana principal
+        JPanel panel = new JPanel();
+        ventana.add(panel);
+        
+        
+        /*3. Dentro del panel, añade una JLabel con el texto "Introduce tu nombre:" y un JTextField de una sola línea
+        configurado con un ancho de, por ejemplo, 20 columnas*/
+        JLabel label_ventana = new JLabel("Introduce tu nombre:");
+        JTextField campoTexto = new JTextField(20);
+        
+        
+        /*4. Añade un JButton con el texto "Saludar". Añade otra JLabel vacía en la parte inferior del panel que servirá
+        para mostrar el resultado*/
+        JButton boton_saludo = new JButton("Saludar");
+        JLabel label_inferior = new JLabel();
+        
+        
+        
+        panel.add(label_ventana);
+        panel.add(campoTexto);
+        panel.add(boton_saludo);
+        panel.add(label_inferior);
+        ventana.setVisible(true);
         System.out.println("Hello World!");
     }
 }
