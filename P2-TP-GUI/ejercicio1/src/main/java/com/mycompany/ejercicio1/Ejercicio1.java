b072eae4cc460ba9bc71171f3f7463f638504eb4
Pablo Aguilar <paedmaag@gmail.com>
2026-05-19 17:45:08 -0300

    feat: se realizó el punto 5 del primer ejercicio, utilizando un action Listener
    -El Listener pertenece al boton de saludo, y en cuanto se toque al boton este devolvera un texto ingresado concatenado con un string

--- P2-TP-GUI/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
+++ P2-TP-GUI/ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.ejercicio1;
 import javax.swing.* ; //libreria principal de GUIs
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  *
  * @author PC
@@ -11,34 +13,47 @@ import javax.swing.* ; //libreria principal de GUIs
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        //1. Crea una ventana principal instanciando un JFrame y asígnale un tamaño usando setSize().
+        //EJ1. Crea una ventana principal instanciando un JFrame y asígnale un tamaño usando setSize().
         //instanciacion de frame
         JFrame ventana = new JFrame();
         ventana.setSize(800,800);
         
-        //2. Crea un contenedor intermedio JPanel y añádelo a la ventana principal
+        //EJ2. Crea un contenedor intermedio JPanel y añádelo a la ventana principal
         JPanel panel = new JPanel();
         ventana.add(panel);
         
         
-        /*3. Dentro del panel, añade una JLabel con el texto "Introduce tu nombre:" y un JTextField de una sola línea
+        /*EJ3. Dentro del panel, añade una JLabel con el texto "Introduce tu nombre:" y un JTextField de una sola línea
         configurado con un ancho de, por ejemplo, 20 columnas*/
         JLabel label_ventana = new JLabel("Introduce tu nombre:");
         JTextField campoTexto = new JTextField(20);
         
         
-        /*4. Añade un JButton con el texto "Saludar". Añade otra JLabel vacía en la parte inferior del panel que servirá
+        /*EJ4. Añade un JButton con el texto "Saludar". Añade otra JLabel vacía en la parte inferior del panel que servirá
         para mostrar el resultado*/
         JButton boton_saludo = new JButton("Saludar");
-        JLabel label_inferior = new JLabel();
-        
+        JLabel label_boton = new JLabel();
         
         
         panel.add(label_ventana);
         panel.add(campoTexto);
         panel.add(boton_saludo);
-        panel.add(label_inferior);
-        ventana.setVisible(true);
+        panel.add(label_boton);
+       
+        
+        // EJ5- Añadimos un Listener
+        // Aquí le decimos al botón qué hacer cuando le hagan clic
+        boton_saludo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Capturamos lo que el usuario escribió
+                String nombreIngresado = campoTexto.getText();
+                // Modificamos la etiqueta de resultado
+                label_boton.setText("¡Hola, " + nombreIngresado + "!");
+            }
+        });
+        
+         ventana.setVisible(true);
         System.out.println("Hello World!");
     }
 }
