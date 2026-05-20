eeba5995db2c442e1ac51d6b6573c6dbd2a80085
Pablo Aguilar <paedmaag@gmail.com>
2026-05-19 22:53:17 -0300

    feat: se realizó parcialmente el ejercicio 3
    1- Se creó la ventana principal con su respectivo tamaño, un panel y un boton
    2-Se creó una ventana secundaria que cumple la funcion de una ventana emergente.
    3-Se añadieron checkboxes y un boton con diferentes opciones para seleccionar.
    4- Se implementó la interaccion para que el boton de la ventana principal llame a la ventana secundaria

---
+++ P2-TP-GUI/Ejercicio3/src/main/java/com/mycompany/ejercicio3/Ejercicio3.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio3;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Pablo
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        /*1-Ventana Principal con sus componentes*/
+        JFrame window = new JFrame();
+        window.setSize(300,300);
+        JPanel windowPanel = new JPanel();
+        JLabel optionLabel = new JLabel("Opciones seleccionadas: Ninguna");
+        JButton preferenceButton = new JButton("Configurar Preferencias");
+        
+        //4- Interaccion para abrir dialogo
+        preferenceButton.addActionListener(new ActionListener(){
+        @Override
+            public void actionPerformed(ActionEvent e) {
+                // instancio la ventana secundaria. 
+                // Le pasamos la ventana principal como "dueña" y la etiqueta para que pueda modificarla.
+                dialogWindow dialogo = new dialogWindow(window, optionLabel);
+                dialogo.setVisible(true); // abre la ventana modal
+            }
+    });
+        window.add(windowPanel);
+        windowPanel.add(optionLabel);
+        windowPanel.add(preferenceButton);
+        window.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
+/*2-Ventana secundaria con sus componentes*/
+        class dialogWindow extends JDialog{
+            public dialogWindow(JFrame window, JLabel updateLabel){
+                super(window, "Preferencias del Sistema", true);
+                setSize(200,200);
+                
+                JPanel dialogPanel = new JPanel();
+                
+                //3- Añadido de checkboxes y boton
+                JCheckBox chkDarkMode = new JCheckBox("Modo Oscuro");
+                JCheckBox chkNotifications = new JCheckBox("Notificaciones");
+                JCheckBox chkAutoSave = new JCheckBox("Autoguardado");
+                JButton btnAccept = new JButton("Aceptar");
+                
+                dialogPanel.add(chkDarkMode);
+                dialogPanel.add(chkNotifications);
+                dialogPanel.add(chkAutoSave);
+                dialogPanel.add(btnAccept);
+                
+                add(dialogPanel);
+                
+            }
+        };
