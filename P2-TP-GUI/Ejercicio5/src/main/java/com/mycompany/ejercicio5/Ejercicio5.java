367141ef6b7b662dff62d447cf6f39bf62106ac2
Pablo Aguilar <paedmaag@gmail.com>
2026-05-20 00:42:21 -0300

    feat: se realizó la prueba de la GUI del ejercicio 5

---
+++ P2-TP-GUI/Ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio5;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+  
+    }
+}
